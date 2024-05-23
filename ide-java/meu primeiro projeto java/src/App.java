9826fc5cb4cac361df277023c612cee189b48c1c
GenesonFerreira <jgeneson@gmail.com>
2024-05-22 22:45:36 -0300

    Adicionando o meu Markdown no meu repositorio Git

---
+++ ide-java/meu primeiro projeto java/src/App.java
@@ -0,0 +1,13 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        int x;
+        x = 2;
+        if (x >= 4) {
+            System.out.println("ola");
+            
+        } else  {
+
+            System.out.println("Ola, pessoa");
+        }
+    }
+}
